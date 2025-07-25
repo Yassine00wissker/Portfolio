a0f3e051f75d6c121673070604978bd28ff749a7
unknown <yassineyahyaoui40@gmail.com>
2025-07-25 19:07:03 +0100

    nave barre creation and mobile menu initialitation

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import LoadingPage from './components/LoadingPage'
 import './App.css'
 import "./index.css"
+import NaveBar from './components/NaveBar'
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <>
-     {!isLoaded && <LoadingPage onComplete={() => setIsLoaded(true)} />}
+     {!isLoaded && <LoadingPage onComplete={() => setIsLoaded(true)} />}{""}
+      <div className={`min-h-screen transition-opacity duration-1000
+        ${isLoaded ? "opacity-100" : "opacity-0"}
+        bg-black text-gray-100`}
+        >
+          <NaveBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      
+      </div>
     </>
   )
 }
---
+++ frontEnd/src/components/NaveBar.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react'
+
+function NaveBar({menuOpen ,setMenuOpen}) {
+
+    useEffect(()=>{
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    },[menuOpen])
+
+  return (
+    <nav className='fixed-top top-0 w-full z-40 bg-black backdrop-blur-lg border-white/10 shadow-lg '>
+        <div id='section' className='max-w-5xl mx-auto px-4'>
+            <div  className='flex justify-between items-center h-16'>
+                <a href='#home' className='font-mono text-xl font-bold text-white'> 
+                    Yassine
+                    <span className='text-blue-500'>.tech</span> </a>
+            <div className='w-7 h-5 relative cursor-pointer z-40 md:hidden' onClick={()=>setMenuOpen(!menuOpen)}>
+                &#9776;
+            </div>
+
+
+
+            <div className='hidden md:flex items-center space-x-8'>
+                <a href='#home' className='text-gray-300 hove:text-white transition-colors'> Home </a>
+            </div>
+
+            <div className='hidden md:flex items-center space-x-8'>
+                <a href='#home' className='text-gray-300 hove:text-white transition-colors'> About </a>
+            </div>
+
+            <div className='hidden md:flex items-center space-x-8'>
+                <a href='#home' className='text-gray-300 hove:text-white transition-colors'> Projects </a>
+            </div>
+
+            <div className='hidden md:flex items-center space-x-8'>
+                <a href='#home' className='text-gray-300 hove:text-white transition-colors'> Contact </a>
+            </div>
+
+
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default NaveBar
