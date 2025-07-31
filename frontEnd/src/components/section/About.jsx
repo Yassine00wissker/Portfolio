e6fef90ad6849f0eaefb7a8ca3ed68b4984266c2
unknown <yassineyahyaoui40@gmail.com>
2025-07-31 21:28:10 +0100

    reveal on scroll animation

--- frontEnd/src/components/section/About.jsx
+++ frontEnd/src/components/section/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import RevealOnScroll from '../RevealOnScroll'
 
 function About() {
     const fontendSkills = [
@@ -19,68 +20,70 @@ function About() {
     return (
         <section id="about"
             className='min-h-screen flex items-center justify-center py-20'>
-            <div className='max-w-3xl mx-auto px-4'>
-                <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'>
-                    About Me
-                </h2>
-                <div className='glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all'>
-                    <p>
-                        🚀 Full-Stack Developer building smart web apps with MERN + AI
-                        <br />🧠 Learning Machine Learning with Python & real-world AI deployment
-                        <br />🔨 Currently working on an AI-powered Product Recommendation Engine
-                    </p>
+            <RevealOnScroll>
+                <div className='max-w-3xl mx-auto px-4'>
+                    <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'>
+                        About Me
+                    </h2>
+                    <div className='glass rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all'>
+                        <p>
+                            🚀 Full-Stack Developer building smart web apps with MERN + AI
+                            <br />🧠 Learning Machine Learning with Python & real-world AI deployment
+                            <br />🔨 Currently working on an AI-powered Product Recommendation Engine
+                        </p>
 
-                    <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-                        <div className='rounded-xl p-6 hover:-translate-y-1 transition-all'>
-                            <h3 className='text-xl font-bold mb-4'>FrontEnd</h3>
-                            <div className='flex flex-wrap gap-2'>
-                                {fontendSkills.map((tech, key) => (
-                                    <span key={key} className='bg-blue-500/10 text-blue-500 px-3 rounded-full py-1 text-sm hover:bg-blue-500/20
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                            <div className='rounded-xl p-6 hover:-translate-y-1 transition-all'>
+                                <h3 className='text-xl font-bold mb-4'>FrontEnd</h3>
+                                <div className='flex flex-wrap gap-2'>
+                                    {fontendSkills.map((tech, key) => (
+                                        <span key={key} className='bg-blue-500/10 text-blue-500 px-3 rounded-full py-1 text-sm hover:bg-blue-500/20
                                 hover:shadow-[0_2px_8px_rgba(50,130,2246,0.2)] transition' >
-                                        {tech}
-                                    </span>
-                                ))}
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
 
-                        <div className='rounded-xl p-6 hover:-translate-y-1 transition-all'>
-                            <h3 className='text-xl font-bold mb-4'>BackEnd</h3>
-                            <div className='flex flex-wrap gap-2'>
-                                {backendSkills.map((tech, key) => (
-                                    <span key={key} className='bg-blue-500/10 text-blue-500 px-3 rounded-full py-1 text-sm hover:bg-blue-500/20
+                            <div className='rounded-xl p-6 hover:-translate-y-1 transition-all'>
+                                <h3 className='text-xl font-bold mb-4'>BackEnd</h3>
+                                <div className='flex flex-wrap gap-2'>
+                                    {backendSkills.map((tech, key) => (
+                                        <span key={key} className='bg-blue-500/10 text-blue-500 px-3 rounded-full py-1 text-sm hover:bg-blue-500/20
                                 hover:shadow-[0_2px_8px_rgba(50,130,2246,0.2)] transition' >
-                                        {tech}
-                                    </span>
-                                ))}
+                                            {tech}
+                                        </span>
+                                    ))}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-8' >
-                    <div className='p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all'>
-                        <h3 className='text-xl font-bold mb-4'> Education </h3>
-                        <ul className='list-disc list-inside text-gray-300 space-y-2'>
-                            <li>
-                                <strong>B.S. information system development</strong>- iset zaghouan (2026)
-                            </li>
-                        </ul>
-                    </div>
-                    <div className='p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all'>
-                        <h3 className='text-xl font-bold mb-4'> Work Experience </h3>
-                        <div className='text-gray-300 space-y-4'>
-                            <div>
-                                <h4>#######################</h4>
-                                <p>###########################
-                                    #########
-                                </p>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-8' >
+                        <div className='p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all'>
+                            <h3 className='text-xl font-bold mb-4'> Education </h3>
+                            <ul className='list-disc list-inside text-gray-300 space-y-2'>
+                                <li>
+                                    <strong>B.S. information system development</strong>- iset zaghouan (2026)
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all'>
+                            <h3 className='text-xl font-bold mb-4'> Work Experience </h3>
+                            <div className='text-gray-300 space-y-4'>
+                                <div>
+                                    <h4>#######################</h4>
+                                    <p>###########################
+                                        #########
+                                    </p>
+                                </div>
+
                             </div>
-
                         </div>
                     </div>
-                </div>
 
-            </div>
+                </div>
+            </RevealOnScroll>
         </section>
     )
 }
--- frontEnd/src/components/section/Home.jsx
+++ frontEnd/src/components/section/Home.jsx
@@ -1,28 +1,31 @@
 import React from 'react'
+import RevealOnScroll from '../RevealOnScroll'
 
 function Home() {
-  return (
-    <section id="home"
-    className='min-h-screen flex items-center justify-center relative'>
-        <div className='text-center z-10 px-4'>
-            <h1 className='text-7xl md:text-6xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text leading-right text-transparent'>
-                Hello! I'm Yassin
-            </h1>
-            <p className='text-gray-400 text-lg mb-8 max-w-lg mx-auto'>
-                an IT student focused on full-stack development , DevOps tools, and AI projects. Currently working on real-world apps .
-            </p>
-            <div className='flex justify-center space-x-4'>
-                <a href='#projects' className='bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 
+    return (
+        <section id="home"
+            className='min-h-screen flex items-center justify-center relative'>
+            <RevealOnScroll>
+                <div className='text-center z-10 px-4'>
+                    <h1 className='text-7xl md:text-6xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text leading-right text-transparent'>
+                        Hello! I'm Yassin
+                    </h1>
+                    <p className='text-gray-400 text-lg mb-8 max-w-lg mx-auto'>
+                        an IT student focused on full-stack development , DevOps tools, and AI projects. Currently working on real-world apps .
+                    </p>
+                    <div className='flex justify-center space-x-4'>
+                        <a href='#projects' className='bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 
                 hover:shadow-2xs'>
-                    View Projects</a> 
+                            View Projects</a>
 
-                <a href='#contact' className='border border-blue-500/50  text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 
+                        <a href='#contact' className='border border-blue-500/50  text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 
                 hover:shadow-2xl hover:bg-blue-500/10'>
-                    Contact Me</a>
-            </div>
-        </div>
-    </section>
-)
+                            Contact Me</a>
+                    </div>
+                </div>
+            </RevealOnScroll>
+        </section>
+    )
 }
 
 export default Home
