175a8713942f19dd59648fe22be13fbca91bd020
Yassine00wissker <yassineyahyaoui40@gmail.com>
2026-03-26 22:02:29 +0100

    Add gh-pages dependency and update deployment scripts; configure Vite base URL for GitHub Pages

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"https://github.com/Yassine00wissker/Portfolio.git",
   plugins: [react(), tailwindcss()],
 })
